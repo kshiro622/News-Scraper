cc6e30491cba7fa27432538a0432ef0b534d7564
Katherine Shiro <katie.shiro@gmail.com>
2017-02-25 15:45:18 -0600

    link to fix heroku favicon error

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var methodOverride = require('method-override');
 var mongoose = require("mongoose");
-var favicon = require("serve-favicon");
 
 // Set up db
 mongoose.connect('mongodb://heroku_q4t0bt76:6veksvknmr4f8q6ec7clegqkl2@ds161059.mlab.com:61059/heroku_q4t0bt76');
@@ -24,18 +23,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+// Set up a static folder (public) for our web app
+app.use(express.static("public"));
+
 // Override with POST having ?_method=DELETE
 app.use(methodOverride('_method'));
-
-// Favicon
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
 // Set Handlebars.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-// Set up a static folder (public) for our web app
-app.use(express.static("public"));
 
 // Routes
 require("./controllers/scraper_controller.js")(app);
